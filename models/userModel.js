a16a4f1688f852746dc62bcebf2056aea5c4898a
fithamlak <justiceofgod01@gmail.com>
2024-01-29 18:59:05 +0300

    add validation confirm password

--- models/userModel.js
+++ models/userModel.js
@@ -4,28 +4,35 @@ const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: [true, "please tell us your name"]
+      type: String,
+      required: [true, "please tell us your name"]
     },
     email: {
-        type: String,
-        required: [true, 'please provide your email'],
-        unique: true,
-        lowercase:true,
-        validate: [validator.isEmail, 'please provide a valid email']
+      type: String,
+      required: [true, 'please provide your email'],
+      unique: true,
+      lowercase:true,
+      validate: [validator.isEmail, 'please provide a valid email']
     },
     photo: String,
     password: {
-        type: String,
-        required: [true, 'plrovide a password'],
-        minLength:8
+      type: String,
+      required: [true, 'plrovide a password'],
+      minLength:8
     },
     passswordConfirm: {
-        type:String,
-        required: [true, 'please confirm your password']
+      type:String,
+      required: [true, 'please confirm your password'],
+      validate: {
+        // this only works on create and save
+        validator: function(el) {
+          return el === this.password;
+        },
+      message: 'not the same password'  
+    },
+    
     }
 })
-
 
 const User = mongoose.model('User', userSchema);
 
