bf777b273bb74de98ef255d5dd7fd57283759eac
fithamlak <justiceofgod01@gmail.com>
2024-01-29 19:43:50 +0300

    make ready usermodel to be used with secure password

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 const validator = require('validator');
 
 
@@ -33,6 +34,17 @@ const userSchema = new mongoose.Schema({
     
     }
 })
+
+userSchema.pre('save', async function(next) {
+  // only run this function if password was actually modified
+  if(!this.isModified('password')) return next();
+  
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12)
+
+  // Delete passwordConfirm field
+  this.passswordConfirm = undefined;
+});
 
 const User = mongoose.model('User', userSchema);
 
