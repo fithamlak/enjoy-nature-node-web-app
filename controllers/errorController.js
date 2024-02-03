c0393f90a365b90f6c309fc2f94c3e088d572762
fithamlak <justiceofgod01@gmail.com>
2024-02-03 03:51:35 +0300

    define custom errors that will be returned for client durring production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -9,7 +9,7 @@ const handleDuplicateFieldsDB = err => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
   console.log(value);
 
-  const message = `Duplicate field value: ${value}. Please use another value!`;
+  const message = `Duplicate field value: . Please use another value!`;
   return new AppError(message, 400);
 };
 const handleValidationErrorDB = err => {
@@ -18,6 +18,10 @@ const handleValidationErrorDB = err => {
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
+
+const handleJWTError = err => new AppError('invalid token. please login again!', 401)
+
+const handleJWTExpiredError = err => new AppError('Your token is expired, please login again', 401)
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -64,6 +68,8 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError(error)
+    if(error.name === 'TokenExpiredError') error = handleJWTExpiredError(error)
 
     sendErrorProd(error, res);
   }
