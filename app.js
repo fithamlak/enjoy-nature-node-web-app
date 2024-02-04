b1b865285a3a82e3ea8a94f0468d6aa9914eb286
fithamlak <justiceofgod01@gmail.com>
2024-02-04 16:56:17 +0300

    Apply helmet middleware to set about 14 http security headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,6 +10,7 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// Set http security headers
 // 1) Global MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -20,16 +22,21 @@ const limter = rateLimit({
   message: 'Too many request fro this IP. please try later after an hour'
 })
 
+// rate limiter to protect from brute force and DDoS attack
 app.use('/api', limter);
-app.use(express.json());
+// parse req. body and also limit size of paylod(body data to be parsed)
+app.use(express.json({limit: '10kb'}));
+
+// middleware for static path
 app.use(express.static(`${__dirname}/public`));
 
-
+// middleware for testing
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
   next();
 });
 
+// middleware for testing
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
