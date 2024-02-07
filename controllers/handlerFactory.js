513a4fdda3db5adf3a85312cf9b46117a2267787
fithamlak <justiceofgod01@gmail.com>
2024-02-07 17:59:13 +0300

    add reusable function to create a document

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -32,3 +32,15 @@ exports.updateOne = Model => catchAsync(async (req, res, next) => {
   });
 
 });
+
+exports.createOne = Model => catchAsync(async (req, res, next) => {
+
+  const newDoc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: newDoc
+      }
+    });
+});
