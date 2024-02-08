1c235b3fd4801b47e71e561b08cda941e039c729
fithamlak <justiceofgod01@gmail.com>
2024-02-09 01:35:27 +0300

    see explain statics result when we didn't use indexing

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -75,6 +75,7 @@ exports.getAll = (Model) => catchAsync(async (req, res, next) => {
     .sort()
     .limitFields()
     .paginate();
+  // const docs = await features.query.explain();
   const docs = await features.query;
 
   // SEND RESPONSE
