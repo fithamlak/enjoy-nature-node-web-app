d33c03dffbfc462e43bddf17df88c82dd8524999
fithamlak <justiceofgod01@gmail.com>
2024-02-08 19:40:46 +0300

    implement middelware to use tourid and user who create review for a tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -22,7 +22,12 @@ exports.getAllReview = catchAsync(async (req, res, next) => {
     }
   });
 });
+exports.setTourUserIds = (req, res, next) => {
+  if(!req.body.tour) req.body.tour = req.params.tourId
+  if(!req.body.user) req.body.user = req.params.user
 
+  next()
+}
 exports.createReview = factory.createOne(Review)
 exports.deleteReview = factory.deleteOne(Review);
 exports.updateReview = factory.updateOne(Review);
